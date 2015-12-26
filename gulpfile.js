8bfee3bf911328ad2563ee486104592e143f2771
Lauriane Kayungu-N <lauriane.kayungu@gmail.com>
2015-12-26 11:46:00 +0000

    Setting up the backbone of the app

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('default', function() {
+  console.log('Hello Zell');
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/app.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('dist'));
+});
