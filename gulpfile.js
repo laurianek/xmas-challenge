f56c70c7d7c7af68cb2abf5638edb51419da2926
Lauriane Kayungu-N <lauriane.kayungu@gmail.com>
2015-12-28 10:37:06 +0000

    more configurations

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
 
 const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const watch = require('gulp-watch');
+const taskListing = require('gulp-task-listing');
 
-gulp.task('default', function() {
-  console.log('Hello Zell');
-});
+gulp.task('default', taskListing);
 
 gulp.task('js', function() {
-  return gulp.src('src/app.js')
+  return gulp.src('app/**/*.js')
+    .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['es2015']
     }))
+    .pipe(concat('main.js'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('watch', function (cb) {
+  var watcher = gulp.watch('app/**/*.js', ['js']);
+  watcher.on('change', function (event) {
+    console.log('Event type: ' + event.type); // added, changed, or deleted
+    console.log('Event path: ' + event.path); // The path of the modified file
+  });
+});
+
