7070b99185382626f107da83ed5b4e3122eb4e1f
Lauriane Kayungu-N <lauriane.kayungu@gmail.com>
2015-12-29 11:15:50 +0000

    wip

--- app/js/grid.js
+++ app/js/grid.js
@@ -1,14 +1,14 @@
 'use strict';
 
 class Grid {
-  constructor(row = 3, col = 3) {
-    this.row = row;
-    this.col = col;
+  constructor(_row = 3, _col = 3) {
+    this.row = _row;
+    this.col = _col;
     this.grid = [];
-    for (let y = 0; y < this.col; y++) {
-      this.grid[y] = [];
-      for (let x = 0; x < this.row; x++) {
-        this.grid[y][x] = null;
+    for (let row = 0; row < this.col; row++) {
+      this.grid[row] = [];
+      for (let col = 0; col < this.row; col++) {
+        this.grid[row][col] = null;
       }
     }
   }
@@ -16,12 +16,12 @@ class Grid {
     if (!this.withinGridBounds(position)) {
       return false;
     }
-    this.grid[position.y][position.x] = marker;
+    this.grid[position.row][position.col] = marker;
     return true;
   }
   withinGridBounds(position) {
-    return (position.x >= 0 && position.x < this.row) &&
-      (position.y >= 0 && position.y < this.col);
+    return (position.row >= 0 && position.row < this.row) &&
+      (position.col >= 0 && position.col < this.col);
   }
   draw() {
 
