3ca385e28c140a175d91dc4ab8154c31115f53e8
Lauriane Kayungu-N <lauriane.kayungu@gmail.com>
2015-12-30 14:13:12 +0000

    wrap colour service

--- app/js/colourservice.js
+++ app/js/colourservice.js
@@ -1,16 +1,19 @@
 'use strict';
 
-class ColourService {
-  getSymbolColour(obj, players) {
-    if(obj.player) {
-      return `symbol-${obj.player.colour}`;
+(function(){
+  class ColourService {
+    getSymbolColour(obj, players) {
+      if(obj.player) {
+        return `symbol-${obj.player.colour}`;
+      }
+      if(obj.marker) {
+        let player = players[0].marker.symbol == obj.marker.symbol ? players[0] : players[1];
+        return `symbol-${player.colour}`;
+      }
+      return '';
     }
-    if(obj.marker) {
-      let player = players[0].marker.symbol == obj.marker.symbol ? players[0] : players[1];
-      return `symbol-${player.colour}`;
-    }
-    return '';
   }
-}
 
-app.service('ColourService', ColourService);
+  app.service('ColourService', ColourService);
+})();
+
